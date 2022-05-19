1d549e408e14784c52230995bdb3936a92b41e3d
Adnane AIT OUSSAYER <adnane.oussayer@gmail.com>
2022-05-19 14:03:51 +0200

    initial commit

---
+++ my_project7/features/build_features.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Feature building
+"""
+
+# import project config.py
+import my_project7.config as cfg
---
+++ my_project7/visualization/visualize.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Visualization
+"""
+# import project config.py
+import my_project7.config as cfg
